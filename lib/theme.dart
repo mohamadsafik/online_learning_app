0c5b037819f7e4db35da6f9a4a266f27f1d44b10
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-02-14 23:27:35 +0700

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,52 @@
+import 'export.dart';
+
+//Colors
+const Color kPrimaryColor = Color(0xffED714D);
+const Color kBlackColor = Color(0xff1F1449);
+const Color kTabColor = Color(0xffEDF5F9);
+const Color kWhiteColor = Color(0xffFFFFFF);
+const Color kGreyColor = Color(0xffBFBFBF);
+const Color kGreenColor = Color(0xff0EC3AE);
+const Color kRedColor = Color(0xffEB70A5);
+const Color kBackgroundColor = Color(0xffF1F1F1);
+const Color kInactiveColor = Color(0xffDBD7EC);
+const Color kAvailableColor = Color(0xffE0D9FF);
+const Color kUnavailableColor = Color(0xffEBECF1);
+const Color kTransparentColor = Colors.transparent;
+
+TextStyle blackTextStyle = GoogleFonts.plusJakartaSans(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.plusJakartaSans(
+  color: kWhiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.plusJakartaSans(
+  color: kGreyColor,
+);
+
+TextStyle orangeTextStyle = GoogleFonts.plusJakartaSans(
+  color: kPrimaryColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.plusJakartaSans(
+  color: kGreenColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.plusJakartaSans(
+  color: kRedColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.plusJakartaSans(
+  color: kPrimaryColor,
+);
+
+//Font Weight
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
