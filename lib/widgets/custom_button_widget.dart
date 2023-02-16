861b2951c52a20d48ee810c8e3e829b3a6bc63fc
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-02-17 00:40:24 +0700

    add custom_button widget

---
+++ lib/widgets/custom_button_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final double width;
+  final double marginTop;
+  final double marginRight;
+  final double marginLeft;
+  final double marginBottom;
+  final double paddingTop;
+  final double paddingRight;
+  final double paddingLeft;
+  final double paddingBottom;
+  final double borderRadius;
+  final double height;
+  final Color backgroundColor;
+  final Color textColor;
+  final TextStyle style;
+  final Function() onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.title,
+    required this.onPressed,
+    this.width = 200,
+    this.marginTop = 0,
+    this.marginBottom = 0,
+    this.marginLeft = 0,
+    this.marginRight = 0,
+    this.paddingTop = 0,
+    this.paddingBottom = 0,
+    this.paddingLeft = 0,
+    this.paddingRight = 0,
+    this.borderRadius = 0,
+    this.height = 40,
+    this.backgroundColor = Colors.amber,
+    this.textColor = Colors.white,
+    this.style = const TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+      fontSize: 14,
+    ),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: marginTop,
+        bottom: marginBottom,
+        left: marginLeft,
+        right: marginRight,
+      ),
+      padding: EdgeInsets.only(
+        top: paddingTop,
+        bottom: paddingBottom,
+        left: paddingLeft,
+        right: paddingRight,
+      ),
+      height: height,
+      width: width,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: backgroundColor,
+          foregroundColor: textColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: style,
+        ),
+      ),
+    );
+  }
+}
