9cc150c2e8424456fae9d77bba580e2b7b4d9c22
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-02-23 15:15:49 +0700

    delete password field for storing to firestore

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -3,9 +3,11 @@ import 'package:online_learning_app/export.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  static bool _obscureText = true;
+  static TextEditingController loginPasswordC = TextEditingController();
+  static TextEditingController loginEmailC = TextEditingController();
 
-  Future registerUser({
+  Future registerUser(
+    BuildContext context, {
     required String name,
     required String email,
     required String role,
@@ -23,23 +25,97 @@ class AuthService {
           "name": name,
           "email": email,
           "role": role,
-          "password": password,
         });
       }
 
       await _userCredential.user!.sendEmailVerification();
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('weak-password');
-      } else if (e.code == 'email-already-in-use') {
-        print('Email already exist');
-      } else if (e.code == 'wrong-password') {
-        print('wrong password');
-      } else {
-        print(e.code);
-      }
+      throw FirebaseException(
+        message: e.code.toString(),
+        plugin: e.code.toString(),
+      );
+      // if (e.code == 'weak-password') {
+      //   print('weak-password');
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     SnackBar(
+      //       duration: const Duration(seconds: 3),
+      //       behavior: SnackBarBehavior.floating,
+      //       backgroundColor: kRedColor,
+      //       content: Row(
+      //         children: const [
+      //           Icon(Icons.beenhere_rounded, color: Colors.white),
+      //           SizedBox(width: 12),
+      //           Text('Your Password is too weak'),
+      //         ],
+      //       ),
+      //     ),
+      //   );
+      // } else if (e.code == 'email-already-in-use') {
+      //   print('Email already exist');
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     SnackBar(
+      //       duration: const Duration(seconds: 3),
+      //       behavior: SnackBarBehavior.floating,
+      //       backgroundColor: kRedColor,
+      //       content: Row(
+      //         children: const [
+      //           Icon(Icons.error, color: Colors.white),
+      //           SizedBox(width: 12),
+      //           Text('Email already in use'),
+      //         ],
+      //       ),
+      //     ),
+      //   );
+      // } else if (e.code == 'wrong-password') {
+      //   print('wrong password');
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     SnackBar(
+      //       duration: const Duration(seconds: 3),
+      //       behavior: SnackBarBehavior.floating,
+      //       backgroundColor: kRedColor,
+      //       content: Row(
+      //         children: const [
+      //           Icon(Icons.error, color: Colors.white),
+      //           SizedBox(width: 12),
+      //           Text('Wrong password'),
+      //         ],
+      //       ),
+      //     ),
+      //   );
+      // } else {
+      //   print(e.code);
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     SnackBar(
+      //       duration: const Duration(seconds: 3),
+      //       behavior: SnackBarBehavior.floating,
+      //       backgroundColor: kRedColor,
+      //       content: Row(
+      //         children: [
+      //           const Icon(Icons.error, color: Colors.white),
+      //           const SizedBox(width: 12),
+      //           Text(e.code.toString()),
+      //         ],
+      //       ),
+      //     ),
+      //   );
+      // }
     } catch (e) {
-      rethrow;
+      throw FirebaseException(plugin: e.toString(), message: e.toString());
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(
+      //     duration: const Duration(seconds: 3),
+      //     behavior: SnackBarBehavior.floating,
+      //     backgroundColor: kRedColor,
+      //     content: Row(
+      //       children: [
+      //         const Icon(Icons.error, color: Colors.white),
+      //         const SizedBox(width: 12),
+      //         Text(e.toString()),
+      //       ],
+      //     ),
+      //   ),
+      // );
+      // rethrow;
     }
   }
 }
