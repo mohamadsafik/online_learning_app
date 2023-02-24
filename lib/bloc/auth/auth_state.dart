ebf5c9dcfc8c9833948c414e8db367097185bd20
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-02-24 21:40:14 +0700

    add extends Equatable

--- lib/bloc/auth/auth_state.dart
+++ lib/bloc/auth/auth_state.dart
@@ -1,18 +1,31 @@
 part of 'auth_bloc.dart';
 
 @immutable
-abstract class AuthState {}
+abstract class AuthState extends Equatable {}
 
-class AuthInitial extends AuthState {}
+class AuthInitial extends AuthState {
+  @override
+  List<Object?> get props => [];
+}
 
-class AuthLoading extends AuthState {}
+class AuthLoading extends AuthState {
+  @override
+  List<Object?> get props => [];
+}
 
 class AuthSuccess extends AuthState {
   final String? message;
   AuthSuccess({this.message});
+
+  @override
+  List<Object?> get props => [];
 }
 
 class AuthError extends AuthState {
   final String? message;
+
   AuthError({this.message});
+
+  @override
+  List<Object?> get props => [];
 }
