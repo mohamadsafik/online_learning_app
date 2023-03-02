c0bb1bf13c19cc826115785776901948d343728a
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-03-02 22:08:30 +0700

    add lecturer home screen

---
+++ lib/screens/lecturer/lecturer_home_screen.dart
@@ -0,0 +1,18 @@
+import '../../export.dart';
+
+class LecturerHomeScreen extends StatelessWidget {
+  const LecturerHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            'ini halaman home pengajar',
+          ),
+        ),
+      ),
+    );
+  }
+}
