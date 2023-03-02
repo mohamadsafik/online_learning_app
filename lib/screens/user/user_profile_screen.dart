e1c88505f23f2d39d36f63353e12c77eca7d62d3
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-03-02 21:20:46 +0700

    add user profile screen

---
+++ lib/screens/user/user_profile_screen.dart
@@ -0,0 +1,10 @@
+import 'package:online_learning_app/export.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
