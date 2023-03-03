4d7f53b4394f965eea19b5a0e40b3f18be2dadde
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-03-03 12:04:33 +0700

    add user settings screen

---
+++ lib/screens/user/user_settings_screen.dart
@@ -0,0 +1,14 @@
+import 'package:online_learning_app/export.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Ini adalah halaman settings'),
+      ),
+    );
+  }
+}
