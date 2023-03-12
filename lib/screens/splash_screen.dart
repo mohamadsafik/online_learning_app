a1b8449108e87f44e04733699f56c503ce8de2c4
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-03-12 17:57:19 +0700

    add some change for checkLogin and checkRole using CheckLoginBloc

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -14,23 +14,65 @@ class _SplashScreenState extends State<SplashScreen> {
       const Duration(
         seconds: 5,
       ),
-      () {
-        Navigator.pushNamedAndRemoveUntil(context, '/auth', (route) => false);
-      },
+      () {},
     );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return CustomSplashScreen(
-      logo: 'assets/logo.png',
-      size: 120,
-      // title: 'Ini Title',
-      spacing: 10,
-      fontWeight: semibold,
-      fontSize: 22,
-      backgroundColor: kBackgroundColor,
+    return BlocBuilder<CheckLoginBloc, CheckLoginState>(
+      bloc: context.read<CheckLoginBloc>()..add(CheckUserLoginEvent()),
+      builder: (context, state) {
+        if (state is CheckLoginSuccess) {
+          if (state.user.role == 'admin') {
+            return AdminHomeScreen(
+              arguments: {
+                "uid": state.user.uid,
+                "name": state.user.name,
+                "role": state.user.role,
+                "email": state.user.email,
+              },
+            );
+          } else if (state.user.role == 'author') {
+            return AuthorHomeScreen(
+              arguments: {
+                "uid": state.user.uid,
+                "name": state.user.name,
+                "role": state.user.role,
+                "email": state.user.email,
+              },
+            );
+          } else {
+            return MainScreen(
+              arguments: {
+                "uid": state.user.uid,
+                "name": state.user.name,
+                "role": state.user.role,
+                "email": state.user.email,
+              },
+            );
+          }
+        } else if (state is CheckLoginLoading) {
+          return const Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (state is CheckLoginError) {
+          return AuthScreen();
+        }
+        return CustomSplashScreen(
+          logo: 'assets/logo.png',
+          size: 120,
+          // title: 'Ini Title',
+          spacing: 10,
+          fontWeight: semibold,
+          fontSize: 22,
+          backgroundColor: kBackgroundColor,
+        );
+      },
     );
   }
 }
